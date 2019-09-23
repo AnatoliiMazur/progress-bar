615966f1b803c04595617437ccc22bfca9be40df
kraken <kraken@templatemonster.me>
2019-09-23 22:43:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# progress-bar.js
+> A JavaScript library for Progress Bar.
+
+## Installation
+Connect js/core.js and css/style.css in your html page
+
+## Usage
+Create ```div``` element with class ```my-bar```, end set value in ```data-to``` attribute.
+You also can create element inside **my-bar** with class **line**, and use you own html tags.
+
+## Examples
+- [codepen](https://codepen.io/Tolyan/pen/oNvaeQJ)
+
+## License
+MIT
---
+++ dev/js/core.js
@@ -0,0 +1,50 @@
+(function () {
+	var bars = [];
+
+	// This is simple progress bar
+	function MyProgressBar( element ) {
+		if ( !(element instanceof HTMLElement) ) {
+			throw new Error( "element should be HTMLElement" );
+		}
+		this._element = element;
+		this._line = this._element.querySelector( ".line" );
+
+		if ( !this._line ) {
+			this._line = document.createElement( 'div' );
+			this._line.classList.add( 'line' );
+			this._element.appendChild( this._line );
+		}
+
+		if ( element.hasAttribute( "data-to" ) ) {
+			this.setValue( element.getAttribute( "data-to" ) );
+		} else {
+			this.setValue( 0 );
+		}
+	}
+
+	MyProgressBar.prototype.setValue = function ( value ) {
+		value = parseInt( value, 10 );
+		if ( value > 100 || value < 0 ) {
+			throw new Error( "value unexpected value" );
+		}
+		setTimeout( function () {
+			this._line.style.width = value + "%";
+		}.bind( this ) );
+	};
+
+	if ( !window.MyProgressBar ) {
+		window.MyProgressBar = MyProgressBar
+	}
+
+	if ( !window.progressBarsInstance ) {
+		window.progressBarsInstance = bars;
+	}
+
+	document.addEventListener( "DOMContentLoaded", function () {
+		var elements = document.querySelectorAll( ".my-bar" );
+
+		for (var i = 0; i < elements.length; i++ ) {
+			bars.push( new MyProgressBar( elements[i] ) );
+		}
+	});
+})();
